e2faeed6467f11adb4b13118c014706d341cc32f
michele <michele.giovagnoli1@gmail.com>
2023-05-04 17:22:36 +0200

    Bonus 1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Movie {
     public $duarata;
 
     //COSTRUTTORE
-    public function __construct($titolo, $genere, $dataRilascio, $durata)
+    public function __construct($titolo, Generi $genere, $dataRilascio, $durata)
     {
         $this->titolo = $titolo;
         $this->genere = $genere;
@@ -21,19 +21,43 @@ class Movie {
     public function getAllDate(){
         return $this->titolo . ' ' . $this->genere . ' ' . $this->dataRilascio . ' ' . $this->durata;
     }
+
+    public function getAllGeneri(){
+        return $this->genere?->uno . ' ' . $this->genere?->due . ' ' . $this->genere?->tre;
+    }
 }
 
-//ISTANZA
-$dune = new Movie('Dune', 'Fantascienza', '2021','2:35');
-$Interstellar = new Movie('Interstellar', 'Fantascienza', '2014', '2:49');
+class Generi {
+    public $uno;
+    public $due;
+    public $tre;
 
+    public function __construct($uno, $due, $tre){
+        $this->uno = $uno;
+        $this->due = $due;
+        $this->tre = $tre;
+    }
+
+}
+//ISTANZA
+$duneGeneri = new Generi('Fantascienza', 'Azione', 'Avventura');
+$dune = new Movie('Dune', $duneGeneri, '2021', '2:35');
 var_dump($dune);
+
+$interstellarGeneri = new Generi('Fantascienza', 'Drammatico', 'Avventura');
+$interstellar = new Movie('Interstellar', $interstellarGeneri, '2014', '2:49');
+var_dump($interstellar);
+
 echo ' <hr/>';
 echo $dune->getAllDate();
+echo ' <br/>';
+echo $dune->getAllGeneri();
 echo ' <hr/>';
-var_dump($Interstellar);
+
 echo ' <hr/>';
 echo $Interstellar->getAllDate();
+echo ' <br/>';
+echo $Interstellar->getAllGeneri();
 echo ' <hr/>';
 
 
